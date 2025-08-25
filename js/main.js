df0cee846eac0f99b84e36a118bb797bc4016783
kse1215 <kseoeun1215@gmail.com>
2025-08-25 12:58:49 +0900

    toTop 버튼 적용

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 window.addEventListener("load", function () {
-    // 각가의 요소를 변수에 담아놓는다.
+  // 모든 a태그 기본 동작 막기
+  // const links = this.document.querySelectorAll("a");
+  // links.forEach(function (link) {
+  //   // console.log(link);
+  //   link.addEventListener("click", function (event) {
+  //     event.preventDefault(); // 기본동작 막기 -위로가기 방지
+  //   });
+  // });
+  // 각각의 요소를 변수에 담아놓는다.
   const nav = document.querySelector(".nav");
   const headerBg = document.querySelector(".header-bg");
   // nav에 마우스 엔터 했을때 즉 호버했을때
--- js/modal.js
+++ js/modal.js
@@ -5,4 +5,20 @@ window.addEventListener("load", function () {
   modalClose.addEventListener("click", function () {
     modal.style.display = "none";
   });
+  //   스크롤 이벤트
+  const topBtn = this.document.querySelector(".top-btn");
+  this.window.addEventListener("scroll", function () {
+    // console.log("스크롤시작");
+    if (this.document.documentElement.scrollTop > 200) {
+      topBtn.style.display = "block";
+    } else {
+      topBtn.style.display = "none";
+    }
+  });
+  topBtn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
 });
